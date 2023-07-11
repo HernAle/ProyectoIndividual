364c07f7862b82540b0cc8cd6c174f53df3dd193
HernAle <hac2794@gmail.com>
2023-07-11 06:16:37 -0300

    primer commit

---
+++ README.md
@@ -0,0 +1,83 @@
+<p align=center><img src=https://d31uz8lwfmyn8g.cloudfront.net/Assets/logo-henry-white-lg.png><p>
+
+# DATA SCIENCE - PROYECTO INDIVIDUAL Nº1 
+
+## Machine Learning Operations (MLOps)
+
+<img src="https://user-images.githubusercontent.com/67664604/217914153-1eb00e25-ac08-4dfa-aaf8-53c09038f082.png"  height=300>
+
+### Contexto
+---
+En este proyecto como MLOps Engineer, nos enfrentamos al desafío de desarrollar un sistema de recomendación de peliculas.<br>
+Sin embargo, encontramos que los datos disponibles son de baja calidad y carecen de estructura, de modo que nuestro objetivo es transformar estos datos, establecer procesos automatizados y construir un modelo mínimo viable (MVP).<br>
+A través de la combinación de habilidades en ciencia de datos, nos enfrentamos al desafío de brindar recomendaciones personalizadas y mejorar la experiencia de los usuarios.
+
+### Data Engineering
+---
+Para el proyecto se procedio a efectuar una serie de **`TRANSFORMACIONES`** solicitadas sobre un dataset dado:
+
+- Algunos campos, como **`belongs_to_collection`**, **`production_companies`** y otros (ver diccionario de datos) están anidados, debimos desanidarlos para poder hacer alguna de las consultas de la API.
+- Los valores nulos de los campos **`revenue`**, **`budget`** deben ser rellenados por el número **`0`**.
+- Los valores nulos del campo **`release date`** deben eliminarse.
+- De haber fechas, deberán tener el formato **`AAAA-mm-dd`**, además deberán crear la columna **`release_year`** donde extraerán el año de la fecha de estreno.
+- Crear la columna con el retorno de inversión, llamada **`return`** con los campos **`revenue`** y **`budget`**, dividiendo estas dos últimas **`revenue / budget`**, cuando no hay datos disponibles para calcularlo, deberá tomar el valor **`0`**.
+- Eliminar las columnas que no serán utilizadas, **`video`**,**`imdb_id`**,**`adult`**,**`original_title`**,**`poster_path`** y **`homepage`**.
+
+### API
+---
+Se debio disponibilizar los siguientes endpoints usando el framework **`FastAPI`**:
+
+- def **peliculas_idioma( *`Idioma`: str* )**:
+Se ingresa un idioma y debe devolver la cantidad de películas producidas en ese idioma.
+
+Ejemplo de retorno: *`X` cantidad de películas fueron estrenadas en `idioma`*
+         
+- def **peliculas_duracion( *`Pelicula`: str* )**:
+Se ingresa una pelicula y debe devolver la la duracion y el año.
+
+Ejemplo de retorno: *`X` . Duración: `x`. Año: `xx`*
+
+- def **franquicia( *`Franquicia`: str* )**:
+Se ingresa la franquicia y debe devolver la cantidad de peliculas, ganancia total y promedio
+
+Ejemplo de retorno: *La franquicia `X` posee `X` peliculas, una ganancia total de `x` y una ganancia promedio de `xx`*
+
+- def **peliculas_pais( *`Pais`: str* )**:
+Se ingresa un país y debe devolver la cantidad de peliculas producidas en el mismo.
+
+Ejemplo de retorno: *Se produjeron `X` películas en el país `X`*
+
+- def **productoras_exitosas( *`Productora`: str* )**:
+Se ingresa la productora y debe devolver el revenue total y la cantidad de peliculas que realizo. 
+
+Ejemplo de retorno: *La productora `X` ha tenido un revenue de `x`*
+
+- def **get_director( *`nombre_director`* )**:
+Se ingresa el nombre de un director y debe devolver el nombre de cada película con la fecha de lanzamiento, retorno individual, costo y ganancia de la misma.
+
+
+### Análisis exploratorio de los datos: _(Exploratory Data Analysis-EDA)_
+---
+Con el fin de comprender mejor los datos transformados, se han llevado acabo un EDA sobre los valores del conjunto de datos.<br>
+Este analisi tenia como objetivo encontrar relaciones entre los datos y comprender su importancia.<br>
+Algunas de la tecnicas utilizadas incluyeron examinar estadisticamente las variables numericas, identificar variables categoricas, explorar correlaciones entre variables y analisis basados en los campos.
+
+### Sistema de recomendación
+--- 
+Para lograr realizar el modelo de Machine Learning se utilizo como criterios filtrar el dataset para las peliculas con mayor impacto del publico, de modo que se hizo uso de los campos de votos (mejor puntaje y mayor cantidad de votos)<br>
+Creando una muestra que no tenga problemas de ejecucion por tamaño ni recursos.
+
+Tambien se trabajo en el armado de vectores numericos para poder aplicar el modelo de similitud de coseno.<br>
+Este trabajo no fue sencillo ya que se debio crear primeramente un vector de tipo string con los campos mas relevantes para realizar un sistema de recomendacion.
+
+- def **recomendacion( *`titulo`* )**:
+Se ingresa el nombre de una película y debe devolver una lista de 5 peliculas similares como recomendacion.
+
+### Deployment
+---
+Para el deploy de la API, se hará uso de **`FASTAPI`** junto a la plataforma **`Render`**
+
+### Video
+---
+Para poder observar pasos del proceso y una explicación mas detallada acerca de los trabajado en el dataset, accede al siguiente link:
+
---
+++ main.py
@@ -0,0 +1,146 @@
+from fastapi import FastAPI
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+app = FastAPI(title='Proyeco Indiviual - MLOps | Cristaldo Hernan',
+              description='API de funciones y recomendacion')
+
+#Carga de datasets
+df_movies = pd.read_csv('movies_etl.csv')
+df_eda = pd.read_csv('movies_eda.csv') 
+
+#Funcion ingreso de idioma y retorno la cantidad de peliculas
+@app.get('/peliculas_idioma/({idioma})')
+def peliculas_idioma(idioma):
+    idioma = idioma.lower()
+    if idioma in df_movies['original_language'].values:
+        df_peliculas_filtradas = df_movies[df_movies['original_language'] == idioma]
+        cantidad_peliculas = len(df_peliculas_filtradas)
+        return {'idioma': idioma, 'cantidad':cantidad_peliculas}
+    else:
+        return 'valor invalido'
+
+#Funcion ingreso de titulo y retorno de duracion y anio de la misma
+@app.get('/peliculas_duracion/({titulo})')
+def peliculas_duracion(titulo):
+    titulo = titulo.lower()
+    if titulo in df_movies['title'].values:
+        df_pelicula = df_movies[df_movies['title'] == titulo]
+        runtime = df_pelicula['runtime'].values.item()
+        year = df_pelicula['year'].values.item()
+        return {'titulo': titulo, 'duracion': runtime, 'anio': year}
+    else:
+        return 'valor invalido'
+    
+
+#Funcion ingreso de franquicia y retorno de ganancia total y promedio
+@app.get('/franquicia/({franquicia})')
+def franquicia(franquicia):
+    franquicia = franquicia.lower()
+    ganancia_total = 0
+    if franquicia in df_movies['collection'].values:
+        df_franquicia = df_movies[df_movies['collection'] == franquicia]
+        ganancias = df_franquicia['revenue'].values[:]
+        for ganancia in ganancias:
+            ganancia_total += ganancia
+        return {'franquicia': franquicia,'ganancia total':ganancia_total,'ganancia promedio': ganancia_total / len(ganancias)}
+    else:
+        return 'valor invalido'
+    
+
+#Funcion ingreso de pais y retorno de cantidad de peliculas por pais
+@app.get('/peliculas_pais/({pais})')
+def peliculas_pais(pais):
+    pais = pais.lower()
+    if pais in df_movies['country'].values:
+        df_pais = df_movies[df_movies['country'] == pais]
+        return {'pais': pais,'cantidad de peliculas':len(df_pais)}
+    else:
+        return 'valor invalido'
+    
+#Funcion ingreso de productora y retorno de cantidad de peliculas y ganancia total de la productora
+@app.get('/productoras_exitosas/({productora})')
+def productoras_exitosas(productora):
+    productora = productora.lower()
+    ganancia_total = 0
+    df_movies['companies'] = df_movies['companies'].fillna('')
+    df_productoras = df_movies[df_movies['companies'].str.contains(productora)]
+    if len(df_productoras) != 0:
+        ganancias = df_productoras['revenue'].values[:]
+        for ganancia in ganancias:
+            ganancia_total += ganancia
+        return {'productora': productora, 'cantidad de peliculas': len(df_productoras), 'ganancia total': ganancia_total}
+    else:
+        return 'valor invalido'
+
+    
+
+#Funcion para agregar un prefijo a cada array 
+def agregar_prefijo(array, prefijo):
+    array_nuevo = []
+    for elemento in array:
+        nuevo_elemento = f'{prefijo}:{elemento}'
+        array_nuevo.append(nuevo_elemento)
+    return np.array(array_nuevo)
+
+# Funcion ingreso de director y retorno de titulo, fecha, retorno de inversion, costo y ganancia de cada pelicula
+@app.get('/get_director/({director})')
+def get_director(director):
+    director = director.lower()
+    df_peliculas = df_movies[df_movies['director'] == director]
+    lista = []  # Inicializar lista como una lista vacía
+    if len(df_peliculas) != 0:
+        titulo = df_peliculas['title'].values[:]
+        titulo = agregar_prefijo(titulo, 'titulo')
+        fecha = pd.to_datetime(df_peliculas['release_date'], errors='coerce')
+        fecha = np.datetime_as_string(fecha, unit='D')
+        fecha = agregar_prefijo(fecha, 'fecha de lanzamiento')
+        retorno = df_peliculas['return'].values[:]
+        retorno = agregar_prefijo(retorno, 'retorno')
+        costo = df_peliculas['budget'].values[:]
+        costo = agregar_prefijo(costo, 'costo:')
+        ganancia = df_peliculas['revenue'].values[:]
+        ganancia = agregar_prefijo(ganancia, 'ganancia')
+
+        lista = list(zip(titulo, fecha, retorno, costo, ganancia))
+    return f'director:{director}', lista
+
+
+# Funcion de recomendacion
+
+#Filtramos las filas a recomendar
+df_filtered = df_eda[df_eda['vote_count'] > 30]
+df_sorted = df_filtered.sort_values('vote_average', ascending=False).head(10000).reset_index(drop=True)
+
+#Creamos un campo compuesto por otros campos seleccionados
+df_sorted['str_vector'] = df_sorted['genres'] + ' ' + df_sorted['overview'] + ' ' + df_sorted['companies']  
+df_sorted = df_sorted[['title', 'str_vector','vote_average']]
+df_sorted['str_vector'].fillna('', inplace=True)
+
+#Vectorizamos el campo compuesto
+vectorizer = TfidfVectorizer(max_features=5000)
+matriz_vector = vectorizer.fit_transform(df_sorted['str_vector'])
+
+#Hacemos uso del metodo de similitud de cosenos para hallar valores similares
+cos_sim = cosine_similarity(matriz_vector)
+
+#Funcion de ingreso de titulo y retorno de recomendacion 
+@app.get('/recomendacion/({titulo})')
+def recomendacion(titulo):
+    titulo = titulo.lower()
+    if titulo in df_sorted['title'].values:
+        indices = pd.Series(df_sorted.index, index=df_sorted['title']).drop_duplicates()
+        idx = indices[titulo]
+        scores = list(enumerate(cos_sim[idx]))
+        scores = sorted(scores, key=lambda x:x[1], reverse=True)
+        top_indices = [i[0] for i in scores if i[0] !=idx]
+
+        top_movies = df_sorted['title'].iloc[top_indices[:5]]
+        recomended_movies = ', '.join(top_movies)
+        return {'peliculas recomendadas': recomended_movies}
+    else:
+        return 'La pelicula no se encuentra dentro de la muestra'
